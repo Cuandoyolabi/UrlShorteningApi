3a1d3701cd8ebe532ff0ccac065f35eea5596580
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-08-12 12:35:48 -0600

    Se crea la funcion crearContenidoHistorial que creara todos los contenedores del historial

--- javascript/script.js
+++ javascript/script.js
@@ -24,10 +24,42 @@ async function acortarUrl(url){
 
     const datos = await respuesta.json();
     console.log(datos);
+
+    //crearContenedorHistorial(url, datos);
+
 };
 
 link__btn.addEventListener("click", () => {
     console.log(valorUrl.value)
     acortarUrl(valorUrl.value);
-
 });
+
+
+/* Contenedor - Historial  */
+const historial = document.getElementById("historial-container-id");
+
+function crearContenedorHistorial(urlCompleta, urlRecortada){
+
+    const contenedorUrl = document.createElement("div");
+    const contenedorUrlInicial = document.createElement("h1");
+    const contenedorUrlSeparador = document.createElement("hr");
+    const contenedorUrlFinal = document.createElement("h1");
+    const contenedorUrlBtn = document.createElement("button");
+    
+    contenedorUrl.classList.add("contenedorUrl");
+    contenedorUrlInicial.classList.add("contenedorUrlInicial");
+    contenedorUrlSeparador.classList.add("contenedorUrlSeparador");
+    contenedorUrlFinal.classList.add("contenedorUrlFinal");
+    contenedorUrlBtn.classList.add("contenedorUrlBtn");
+
+    contenedorUrlInicial.textContent = urlCompleta;
+    contenedorUrlFinal.textContent = urlRecortada;
+    contenedorUrlBtn.textContent = "Copy";
+
+    historial.appendChild(contenedorUrl);
+    contenedorUrl.appendChild(contenedorUrlInicial);
+    contenedorUrl.appendChild(contenedorUrlSeparador);
+    contenedorUrl.appendChild(contenedorUrlFinal);
+    contenedorUrl.appendChild(contenedorUrlBtn);
+
+}
